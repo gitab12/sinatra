f11588ea727e5211dee32e8d440f92b8c6bd7800
gitab12 <geethabasavraju18@gmail.com>
2018-09-01 14:30:14 +0530

    Add All Files

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/reloader'
+get '/' do
+  redirect to('/index')
+end
+get '/' do
+  "Hello Frank"
+end
+
+
+  get '/index' do
+  @songs = [{title: 'Fly Me To The Moon', url: "https://www.youtube.com/watch?v=mQR0bXO_yI8"},
+            {title: 'New York, New York', url: "https://www.youtube.com/watch?v=btFfXgUdIzY"},
+            {title: 'My Way', url: 'https://www.youtube.com/watch?v=6E2hYDIFDIU'},
+            {title: 'The Way You Look Tonight', url: 'https://www.youtube.com/watch?v=h9ZGKALMMuc'},
+            {title: 'I Get A Kick Out Of You', url: 'https://www.youtube.com/watch?v=wSrHvNr8QQQ'}]
+  erb :default
+end
+  #"Hello World"
